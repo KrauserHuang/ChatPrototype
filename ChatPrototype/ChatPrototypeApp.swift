d809373d48d61c77cdab1ea3eb5dc1f3960934df
Krauser Huang <crescentdence@gmail.com>
2025-04-12 12:27:04 +0800

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Tai Chin Huang on 2025/4/12.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
